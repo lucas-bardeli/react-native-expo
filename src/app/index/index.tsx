9d1f58eaa754149d685329e13f0ee8aea22ead4b
lucas-bardeli <lucasbbsjau@gmail.com>
2026-08-30 17:04:22 -0300

    Criando o componente Category utilizando props e variação de estilos

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,3 +1,4 @@
+import { Category } from "@/components/category";
 import { colors } from "@/styles/colors";
 import { MaterialIcons } from "@expo/vector-icons";
 import { Image, TouchableOpacity, View } from "react-native";
@@ -13,6 +14,10 @@ export default function Index() {
           <MaterialIcons name="add" size={32} color={colors.green[300]} />
         </TouchableOpacity>
       </View>
+
+      <Category name="Projetos" icon="code" isSelected />
+      <Category name="Sites" icon="language" isSelected={false} />
+      <Category name="Vídeos" icon="movie" isSelected={false} />
     </View>
   );
 }
