9a81f30c15a51d88f5396ce47d7deef94c347b01
lucas-bardeli <lucasbbsjau@gmail.com>
2026-08-30 18:41:34 -0300

    Adicionando categorias em uma utils e renderizando em uma lista

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,4 +1,4 @@
-import { Category } from "@/components/category";
+import { Categories } from "@/components/categories";
 import { colors } from "@/styles/colors";
 import { MaterialIcons } from "@expo/vector-icons";
 import { Image, TouchableOpacity, View } from "react-native";
@@ -15,9 +15,7 @@ export default function Index() {
         </TouchableOpacity>
       </View>
 
-      <Category name="Projetos" icon="code" isSelected />
-      <Category name="Sites" icon="language" isSelected={false} />
-      <Category name="Vídeos" icon="movie" isSelected={false} />
+      <Categories />
     </View>
   );
 }
