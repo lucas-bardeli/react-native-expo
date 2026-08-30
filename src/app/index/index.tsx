f20b6912feca85119dd004ea2b5aaf3f9089651e
lucas-bardeli <lucasbbsjau@gmail.com>
2026-08-30 20:05:49 -0300

    Modal para exibir as informações de um Link

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -2,7 +2,14 @@ import { Categories } from "@/components/categories";
 import { Link } from "@/components/link";
 import { colors } from "@/styles/colors";
 import { MaterialIcons } from "@expo/vector-icons";
-import { FlatList, Image, TouchableOpacity, View } from "react-native";
+import {
+  FlatList,
+  Image,
+  Modal,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { styles } from "./styles";
 
 export default function Index() {
@@ -32,6 +39,26 @@ export default function Index() {
         contentContainerStyle={styles.linksContent}
         showsVerticalScrollIndicator={false}
       />
+
+      <Modal visible transparent>
+        <View style={styles.modal}>
+          <View style={styles.modalContent}>
+            <View style={styles.modalHeader}>
+              <Text style={styles.modalCategory}>Curso</Text>
+              <TouchableOpacity>
+                <MaterialIcons
+                  name="close"
+                  size={20}
+                  color={colors.gray[400]}
+                />
+              </TouchableOpacity>
+            </View>
+
+            <Text style={styles.modalLinkName}>Rocketseat</Text>
+            <Text style={styles.modalUrl}>https://app.rocketseat.com.br/</Text>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
